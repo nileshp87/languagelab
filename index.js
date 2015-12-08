abfc11f1a164f1cba8a0a125ebda3e1c7c19e69f
Nilesh Patel <nileshp88@gmail.com>
2015-12-08 18:06:19 -0500

    week 3 code

--- index.js
+++ index.js
@@ -8,16 +8,25 @@ var redis = require('redis');
 var client = redis.createClient();
 
 app.set('view engine', 'jade');
+app.use(express.static('node_modules/bootstrap/dist'));
 
-var posts = [];
+app.locals.pretty = true;
+
+app.get('/', function(req, res){
+	client.lrange('posts', -4, -1, function(errors, posts){
+		res.render('index', {'posts': posts, 'title': "Home"});
+	});
+});
 
 app.get('/post', function(req, res){
 	res.render('post');
 });
 
 app.post('/post', function(req, res){
-	client.rpush('posts',JSON.stringify({'title': req.body.title, 'content': req.body.content}));
-	res.render('success');
+	client.incr('id', function(errors, id){
+		client.rpush('posts',JSON.stringify({'title': req.body.title, 'content': req.body.content, 'id': id-1}));
+		res.render('success');
+	});
 });
 
 app.get('/posts/:id', function(req, res){
@@ -32,7 +41,7 @@ app.get('/posts/:id', function(req, res){
 
 app.get('/posts', function(req, res){
 	client.lrange('posts',-10, -1, function(errors, posts){
-		res.render('posts', {'posts': posts.reverse()});
+		res.render('posts', {'posts': posts.reverse(), 'title': 'All Posts'});
 	});
 });
 
